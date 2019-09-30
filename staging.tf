ccffc143528731b9f674044d87cfae1c958e94f5
Ben <ben.the.rabbit@gmail.com>
2019-09-30 17:47:25 +0200

    Add firewalling for port 80 change service name to httpd

--- staging.tf
+++ staging.tf
@@ -21,6 +21,12 @@ resource "aws_security_group" "plex" {
 	description = "Allow traffic for Plex Media Server"
 
 	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	ingress {
 		from_port = 443
 		to_port = 443
 		protocol = "tcp"
